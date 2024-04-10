d89ce7b053e0e9a81bd83b591600d61058980446
Dechen Bhuming <db109@wellesley.edu>
2024-04-10 18:52:01 -0400

    added more questions to database

--- data/questions.js
+++ data/questions.js
@@ -9,9 +9,81 @@
 {
 	question: 'has 8:30?',
 	percentage: 15,
-	submissions: 2,
-	userAnswered: ['am114', 'db109'],
+	submissions: 4,
+	userAnswered: ['am114', 'db109', 'fn101', 'sk106'],
+	usersPlayed: [],
+	readyForUse: false,
+},
+],
+
+[{
+	question: 'has swam in Lake Waban?',
+	percentage: 20,
+	submissions: 5,
+	userAnswered: ['am114', 'db109', 'kj123', 'dk342', 'po392'],
 	usersPlayed: ['fn101', 'sk106'],
 	readyForUse: true,
 },
+{
+	question: 'can cry on demand?',
+	percentage: 10,
+	submissions: 4,
+	userAnswered: ['am114', 'db109', 'fn101', 'sk106'],
+	usersPlayed: [],
+	readyForUse: false,
+},
+
+{
+	question: 'live on the east coast?',
+	percentage: 25,
+	submissions: 4,
+	userAnswered: ['am114', 'db109', 'fn101', 'sk106'],
+	usersPlayed: [],
+	readyForUse: false,
+},
+
+{
+	question: 'are scared of clowns?',
+	percentage: 50,
+	submissions: 2,
+	userAnswered: ['am114', 'db109'],
+	usersPlayed: [],
+	readyForUse: false,
+},
+
+{
+	question: 'listen to country music?',
+	percentage: 0,
+	submissions: 1,
+	userAnswered: ['am114'],
+	usersPlayed: [],
+	readyForUse: false,
+},
+
+{
+	question: 'like spicy food?',
+	percentage: 57,
+	submissions: 7,
+	userAnswered: ['am114', 'db109', 'fk100', 'am145', 'zc104', 'tk105', 'sc116'],
+	usersPlayed: ['fn101', 'sk106'],
+	readyForUse: true,
+},
+
+{
+	question: 'have completed their PE requirement',
+	percentage: 22,
+	submissions: 9,
+	userAnswered: ['am114', 'db109', 'fk100', 'am145', 'zc104', 'tk105', 'sc116', 'fn101', 'sk106'],
+	usersPlayed: ['oo100'],
+	readyForUse: true,
+},
+
+{
+	question: 'live on Westside',
+	percentage: 22,
+	submissions: 9,
+	userAnswered: ['am114', 'db109', 'fk100', 'am145', 'zc104', 'tk105', 'sc116', 'fn101', 'sk106'],
+	usersPlayed: ['oo100'],
+	readyForUse: true,
+}
 ]
