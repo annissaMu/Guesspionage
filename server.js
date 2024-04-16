ecc36c2ddae9c873fc0250607dcb8e498934dbcd
Annissa Mu <am114@cs.wellesley.edu>
2024-04-16 10:23:48 -0400

    resolved merge conflict

--- server.js
+++ server.js
@@ -70,35 +70,7 @@ app.get('/', (req, res) => {
     visits++;
     req.session.visits = visits;
     console.log('uid', uid);
-    return res.render('index.ejs', {uid, visits});
-});
-
-app.get('/game/', async (req, res) => {
-    const db = await Connection.open(mongoUri, 'guesspionage');
-    let questions = await db.collection('questions').find().toArray();
-    let questionsList = [];
-    let questionsCounter = 0;
-    let indexList = [];
-    while (questionsCounter < 5) {
-        // keep track of unique indexes
-        while (indexList.length == questionsCounter){
-            let index = Math.floor(Math.random() * questions.length);
-            if (!indexList.includes(index)){
-                indexList.push(index);
-            }
-        }
-    
-        // push ready for use questions into questions list
-        if (questions[indexList[questionsCounter]].readyForUse == true) {
-            questionsList.push(questions[indexList[questionsCounter]]);
-            questionsCounter++;
-        } else {
-            indexList.pop();
-        }
-    }
-    console.log(questionsList);
-
-    return res.render('game.ejs', {questionsList});
+    return res.render('home.ejs', {uid, visits});
 });
 
 app.get('/game/', async (req, res) => {
@@ -107,6 +79,7 @@ app.get('/game/', async (req, res) => {
     let answer3 = req.query.answer3;
     let answer4 = req.query.answer4;
     let answer5 = req.query.answer5;
+    console.log(answer1);
     const db = await Connection.open(mongoUri, 'guesspionage');
     let questions = await db.collection('questions').find().toArray();
     let questionsList = [];
@@ -129,19 +102,22 @@ app.get('/game/', async (req, res) => {
             indexList.pop();
         }
     }
+    // if there's no submission render game, else render the game results
     if (!answer1) {
         return res.render('game.ejs', {questionsList});  
     } else {
-        console.log('redirected!');
-        return res.redirect('/result/', {questionsList, answer1, answer2, answer3, answer4, answer5})
+        // do score calculation here!
+        return res.render('results.ejs', {questionsList, answer1, answer2, answer3, answer4, answer5})
     }
 });
 
-app.get
+app.post('/results/', async (req, res) => {
+    // update leaderboard, display spot on leaderboard (ignoring users)
+})
 
 // shows how logins might work by setting a value in the session
 // This is a conventional, non-Ajax, login, so it redirects to main page 
-app.post('/set-uid/', (req, res) => {
+/*app.post('/set-uid/', (req, res) => {
     console.log('in set-uid');
     req.session.uid = req.body.uid;
     req.session.logged_in = true;
@@ -169,7 +145,7 @@ app.post('/logout/', (req, res) => {
     req.session.uid = false;
     req.session.logged_in = false;
     res.redirect('/');
-});
+});*/
 
 // two kinds of forms (GET and POST), both of which are pre-filled with data
 // from previous request, including a SELECT menu. Everything but radio buttons
